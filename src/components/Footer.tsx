0a941b3294722e5809417c88c163c4683e2b12f4
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-11-10 14:48:10 -0800

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,32 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Pressable } from "react-native";
 import Linking from "expo-linking";
 import { FontAwesome } from "@expo/vector-icons";
 import TextBox from "./TextBox";
+import { useState, useEffect } from "react";
+import AppState from "../stores/AppState";
 
 const openGithub = () => {
   Linking.openURL("https://github.com/pdtru");
 };
 
 const Footer = () => {
+  const [isHovered, setIsHovered] = useState(AppState.hoveredIcon);
+
+  useEffect(() => {
+    const hoveredIconSubscription =
+      AppState.hoveredIconObservable.subscribe(setIsHovered);
+
+    return () => {
+      hoveredIconSubscription.unsubscribe();
+    };
+  }, []);
+
+  const onHoverIn = () => {
+    setIsHovered(true);
+  };
+  const onHoverOut = () => {
+    setIsHovered(false);
+  };
   return (
     <View
       style={{
@@ -17,8 +36,16 @@ const Footer = () => {
       }}
     >
       <TextBox text="Copyright © 2023 pdtru " />
-      <Pressable onPress={openGithub}>
-        <FontAwesome name="github" size={24} color="#6c5292" />
+      <Pressable
+        onPress={openGithub}
+        onHoverIn={onHoverIn}
+        onHoverOut={onHoverOut}
+      >
+        <FontAwesome
+          name="github"
+          size={24}
+          color={isHovered ? "#9d78d2" : "#6c5292"}
+        />
       </Pressable>
     </View>
   );
